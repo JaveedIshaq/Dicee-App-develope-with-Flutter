35b3fa7fd930f74322458eb6a8adaa3eeeeb43e7
Javeed Ishaq <jishaq@gtek.solutions>
2019-12-06 22:25:06 +0500

    app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
 
 void main() {
   return runApp(
@@ -15,9 +17,55 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceImage = 3;
+  int rightDiceImage = 5;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                         leftDiceImage = Random().nextInt(6) + 1;
+                         rightDiceImage = Random().nextInt(6) + 1;
+                      });
+                      print('image 1 pressed');
+                    },
+                    child: Image.asset('images/dice$leftDiceImage.png')),
+              )
+          ),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: FlatButton(
+                    onPressed: (){
+                      setState(() {
+                        leftDiceImage = Random().nextInt(6) + 1;
+                        rightDiceImage = Random().nextInt(6) + 1;
+                      });
+                      print("image 2 pressed");
+
+                      },
+                    child: Image.asset('images/dice$rightDiceImage.png')
+                ),
+              )
+          )
+        ],
+      ),
+    );
   }
 }
